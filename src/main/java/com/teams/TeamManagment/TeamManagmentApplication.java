f850778d0347589d89ba7f550469cd540edf8eb4
Rohit Singh Rajput <rohitsinghrajput0777@gmail.com>
2022-01-27 11:13:09 +0530

    project created

---
+++ src/main/java/com/teams/TeamManagment/TeamManagmentApplication.java
@@ -0,0 +1,13 @@
+package com.teams.TeamManagment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamManagmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeamManagmentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/teams/TeamManagment/TeamManagmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teams.TeamManagment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeamManagmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
